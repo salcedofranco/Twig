b6fc825f5d849d5ccf54b6c2393fea9aae8e90ae
Franco Salcedo <fcedo13@gmail.com>
2022-06-29 15:45:54 -0300

    Primer commit Twig instalacion y primeros pasos

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+$loader = new \Twig\Loader\ArrayLoader([
+    'index' => 'Hello {{ name }} {{ saludo }}!',
+]);
+$twig = new \Twig\Environment($loader);
+
+/*
+$params = [
+    'name' => 'Franklin',
+    'saludo' => 'bienvenido a twig desde params'
+];
+
+//Primer ECHO echo $twig->render('index', ['name' => 'Franklin', 'saludo' => 'bienvenido a twig']);
+
+echo $twig->render('index', $params );
+*/
+
+// METODO COMPACT PHP
+
+$agregado = 'lalala';
+
+$name = 'Franco Salcedo';
+$saludo = 'desde metodo compact ' .  $agregado;
+
+echo $twig->render('index' , compact('name' , 'saludo'));
